9e1fbc1f0f1572942b6c3bafe5c779616bc40ad0
chenmingqiu <1075@qq.com>
2025-03-01 15:24:09 +0800

    feat: 完成向合约转账

--- src/app/contract/page.tsx
+++ src/app/contract/page.tsx
@@ -20,6 +20,7 @@ const ACCOUNT4_ADDRESS = '0x70997970C51812dc3A010C7d01b50e0d17dc79C8';
 const CONTRACT_ADDRESS = '0xa513E6E4b8f2a923D98304ec87F64353C4D5C853';
 
 const Transaction = () => {
+  const [inputValue, handleInputChange] = useState('');
   const [account3, setAccount3] = useState({
     name: 'anvil测试号',
     addr: '',
@@ -35,22 +36,14 @@ const Transaction = () => {
     if (window.ethereum) {
       const provider = new BrowserProvider(window.ethereum);
       setProvider(provider);
-
-      // 初始化钱包
-      initWallet(provider);
-
-      // 初始化账户3
-      const signer = await initAccount(provider);
-      // 初始化合约
-      initContract(signer);
     } else {
       // provider = ethers.getDefaultProvider();
     }
   }
 
-  async function initAccount(provider: BrowserProvider) {
-    const signer = await provider.getSigner();
-    const accountBalance = await provider.getBalance(signer.address);
+  async function initAccount() {
+    const signer = await provider!.getSigner();
+    const accountBalance = await provider!.getBalance(signer.address);
     setAccount3({
       ...account3,
       addr: signer.address,
@@ -59,7 +52,7 @@ const Transaction = () => {
     return signer;
   }
 
-  async function initContract(signer: ethers.JsonRpcSigner) {
+  function initContract(signer: ethers.JsonRpcSigner) {
     const contract = new Contract(CONTRACT_ADDRESS, FUNDME_ABI, signer);
     setContract(contract);
 
@@ -83,6 +76,8 @@ const Transaction = () => {
     const balance = await contract!.getBalance();
     console.log('balance:', formatEther(balance));
     setContractBalance(formatEther(balance));
+
+    initAccount();
   };
   //  获取链上交易次数
   const getTransactionCount = async () => {
@@ -95,22 +90,45 @@ const Transaction = () => {
   const sendToContract = async () => {
     // 将以太币数量转换为 wei
     const value = parseEther('1');
-    console.log('ddd', value);
-    const tx = await contract!.fund({ value });
+    const tx = await contract!.fund({ value }); // 通过对象传参
 
     // 等待交易完成
     const receipt = await tx.wait();
     console.log('转账：', receipt);
   };
 
+  const sendTransaction = async () => {
+    const tx = await wallet!.sendTransaction({
+      to: CONTRACT_ADDRESS,
+      value: parseEther(inputValue)
+    });
+    const receipt = await tx.wait();
+    console.log(receipt);
+  };
+
   const withdraw = async () => {
-    const result = await contract!.withdraw();
-    console.log('提现：', result);
+    const tx = await contract!.withdraw(parseEther('1'));
+    const receipt = await tx.wait();
+    console.log('提现：', receipt);
   };
 
   useEffect(() => {
     init();
   }, []);
+
+  useEffect(() => {
+    if (provider) {
+      (async () => {
+        // 初始化钱包
+        initWallet(provider);
+
+        // 初始化账户3
+        const signer = await initAccount();
+        // 初始化合约
+        initContract(signer);
+      })();
+    }
+  }, [provider]);
 
   useEffect(() => {
     if (contract) getContractBalance();
@@ -122,6 +140,13 @@ const Transaction = () => {
       <InfoCar
         info={{ addr: CONTRACT_ADDRESS, balance: contractBalance }}
         isContract
+      />
+
+      <input
+        className='block border border-solid rounded-xl px-4 py-2 m-4'
+        type='text'
+        value={inputValue}
+        onChange={e => handleInputChange(e.target.value)}
       />
 
       <button
@@ -138,8 +163,14 @@ const Transaction = () => {
 
       <button
         className='border border-teal-700 border-solid rounded-xl px-4 py-2 m-4'
+        onClick={sendTransaction}>
+        发起交易合约转账
+      </button>
+
+      <button
+        className='border border-teal-700 border-solid rounded-xl px-4 py-2 m-4'
         onClick={sendToContract}>
-        向合约转账
+        调用合约向合约转账
       </button>
 
       <button
